45debd8ba750365df6e7f7c6afe4b0cc0a090836
Daniel Rodriguez <df.rodriguez143@gmail.com>
2015-03-12 12:32:28 -0500

    couple of fixes on envs

--- salt/_modules/conda.py
+++ salt/_modules/conda.py
@@ -12,21 +12,29 @@ __func_alias__ = {
     'list_': 'list'
 }
 
+def get_prefix(user=None):
+    if user:
+        for u in pwd.getpwall():
+            if u.pw_name == user:
+                return os.path.join(u.pw_dir, 'anaconda')
+    else:
+        return __salt__['grains.get']('conda:prefix', default='/opt/anaconda')
 
-def create(path, user=None):
+
+def create(name, user=None):
     """
     Create a conda virutalenv
     """
-    cmd = _create_conda_cmd('create', path, ['pip', '--yes', '-q'], user=user)
+    cmd = _create_conda_cmd('create', args=['pip', '--yes', '-q'], env=name, user=user)
     ret = _execcmd(cmd, user=user)
 
     if ret['retcode'] == 0:
         # Virtual enviroment created
-        return 'Virtual enviroment "%s" created' % path
+        return 'Virtual enviroment "%s" created' % name
     else:
         if ret['stderr'].startswith('Error: prefix already exists:'):
             # Virtual env already exists
-            return 'Virtual enviroment "%s" already exists' % path
+            return 'Virtual enviroment "%s" already exists' % name
         else:
             raise salt.exceptions.CommandExecutionError(ret['stderr'])
 
@@ -55,7 +63,12 @@ def list_(env=None, user=None):
     if ret['retcode'] == 0:
         return ret['stdout']
     else:
-        raise 'ERROR: ' + ret['stderr']
+        raise Exception(ret['stderr'])
+
+
+def _install_conda(package, env=None, user=None):
+    cmd = _create_conda_cmd('install', args=[package, '--yes', '-q'], env=env, user=user)
+    return _execcmd(cmd, user=user)
 
 
 def _install_pip(package, env=None, user=None):
@@ -63,40 +76,24 @@ def _install_pip(package, env=None, user=None):
     return _execcmd(cmd, user=user)
 
 
-def _install_conda(package, env=None, user=None):
-    cmd = _create_conda_cmd('install', args=[package, '--yes', '-q'], env=None, user=user)
-    return _execcmd(cmd, user=user)
-
-
 def _create_conda_cmd(conda_cmd, args=None, env=None, user=None):
-    cmd = [_get_conda_path(user=user), conda_cmd]
+    cmd = [_get_conda_path(env=None, user=user), conda_cmd]
     if env:
-        cmd = cmd + ['-n', env]
-    if args is not None and args != []:
+        cmd.extend(['-n', env])
+    if args is not None and type(args) is list and args != []:
         cmd.extend(args)
     return cmd
 
 
-def _get_conda_path(user=None):
-    if user:
-        return os.path.join(_get_anaconda_dir_path(user=user), 'bin', 'conda')
-    return 'conda'
+def _get_conda_path(env=None, user=None):
+    return os.path.join(get_prefix(user=user), 'bin', 'conda')
 
 
 def _get_env_path(env=None, user=None):
-    print user, env
-    if user:
-        if env:
-            return os.path.join(_get_anaconda_dir_path(user=user), 'envs', env)
-        else:
-            return _get_anaconda_dir_path(user=user)
-
-
-def _get_anaconda_dir_path(user=None):
-    if user:
-        for u in pwd.getpwall():
-            if u.pw_name == user:
-                return os.path.join(u.pw_dir, 'anaconda')
+    if env:
+        return os.path.join(get_prefix(user=user), 'envs', env)
+    else:
+        return get_prefix(user=user)
 
 
 def _execcmd(cmd, user=None):
