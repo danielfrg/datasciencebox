c6d6bcd85c9ddf152778d92081cc7ad912d3373f
Daniel Rodriguez <df.rodriguez143@gmail.com>
2015-09-16 17:46:41 -0500

    Test arguments

--- datasciencebox/tests/core/test_instance.py
+++ datasciencebox/tests/core/test_instance.py
@@ -3,7 +3,12 @@ import pytest
 from datasciencebox.core.settings import Settings
 from datasciencebox.core.cloud.instance import Instance, BareInstance, AWSInstance, GCPInstance
 
+default_username = 'default_username'
+default_keypair = 'default_keypair'
+
 settings = Settings()
+settings['USERNAME'] = default_username
+settings['KEYPAIR'] = default_keypair
 
 
 def test_new_bare():
@@ -12,19 +17,25 @@ def test_new_bare():
 
     instance = Instance.new(settings=settings, uid='myid')
     assert instance.uid == 'myid'
-
-    instance = Instance.new(settings=settings, uid='myid', ip='1.1.1.1')
-    assert instance.ip == '1.1.1.1'
     assert instance.port == 22
+    assert instance.username == default_username
+    assert instance.keypair == default_keypair
 
-    assert instance.to_dict() == {'ip': '1.1.1.1', 'port': 22, 'uid': 'myid'}
+    instance = Instance.new(settings=settings, uid='myid', ip='1.1.1.1', port=33, username='me', keypair='mykey')
+    assert instance.ip == '1.1.1.1'
+    assert instance.port == 33
+    assert instance.username == 'me'
+    assert instance.keypair == 'mykey'
+    assert instance.to_dict() == {'ip': '1.1.1.1', 'port': 33, 'uid': 'myid'}
 
 
-def test_new_bare_ssh_port():
+def test_new_bare_ip_with_port():
     instance = Instance.new(settings=settings, uid='myid', ip='1.1.1.1:2022')
     assert isinstance(instance, BareInstance)
     assert instance.ip == '1.1.1.1'
     assert instance.port == 2022
+    assert instance.username == default_username
+    assert instance.keypair == default_keypair
 
 
 def test_new_clouds():
